124501f8d95669c1756e031ac5a1cb693e47e3dd
kendalpasa <kendalpasa@outlook.com>
2025-10-02 21:49:12 +0300

    1.0

---
+++ script.js
@@ -0,0 +1,33 @@
+var viewer = OpenSeadragon({
+            id: "openseadragon1",
+            prefixUrl: "https://cdnjs.cloudflare.com/ajax/libs/openseadragon/3.1.0/images/",
+            showNavigator: true            
+        });
+
+           const apiKey = '5KvmVfJn8h8O67Tqwt8fmTSzXFcN7HzCjQEp7Ogk';
+        const url = `https://api.nasa.gov/planetary/apod?api_key=${"5KvmVfJn8h8O67Tqwt8fmTSzXFcN7HzCjQEp7Ogk"}`;
+
+
+         fetch(url)
+            .then(response => response.json())
+              .then(apodData => {
+                console.log('APOD verisi:', apodData);
+
+                 if (apodData.media_type === 'image') {
+                    viewer.open({
+                        type: 'image',
+                        url: apodData.hdurl || apodData.url
+                    });
+                }
+
+                })
+
+
+getStartedButton.addEventListener('click', function() {
+    const viewerSection =document.getElementById('viewer');
+
+    viewerSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+});
